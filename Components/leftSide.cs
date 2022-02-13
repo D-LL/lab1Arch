792bd887bdc44b174f2e0edaa0d5cd9b6bcd8cf9
D-LL <daniel.lussierlevesque@gmail.com>
2022-02-13 12:20:13 -0500

    simulation frame is probably not the same as unity frame

--- Components/leftSide.cs
+++ Components/leftSide.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LeftSide : IComponent
 {
     public List<EntityComponent> entities = new List<EntityComponent>();
-    public int frame;
+    public uint frame;
     public float deltaTimeRight;
 }
 
